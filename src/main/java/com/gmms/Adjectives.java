4d9817a223835868183d51ddc8af1ef1fbe036d1
mattia-gallinaro <gallinaromattia6@gmail.com>
2025-08-12 18:22:39 +0200

    Aggiunti file per dati e cominciato il dizionario

--- src/main/java/com/gmms/Adjectives.java
+++ src/main/java/com/gmms/Adjectives.java
@@ -1,5 +1,30 @@
 package com.gmms;
+
+import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
 // Mattia Gallinaro
 public class Adjectives {
+    List<String> words;
+    Random random;
 
+    //da testare
+    public String Adjectives(){
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public Adjectives(String[] words){
+        random = new Random();
+    }
+    
+    public String[] getAdjectives(int count){
+        String[] pickedWords = new String[count];
+        List<String> copyList = new ArrayList<String>(words);
+        for(int i  = 0; i < count ; i++){
+            int pos = random.nextInt(copyList.size());
+            pickedWords[i] = copyList.get(pos);
+            copyList.remove(pos);
+        } 
+        return pickedWords;
+    }
 }
--- src/main/java/com/gmms/Nouns.java
+++ src/main/java/com/gmms/Nouns.java
@@ -1,5 +1,30 @@
 package com.gmms;
+
+import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
 // Mattia Gallinaro
 public class Nouns {
+    List<String> words;
+    Random random;
 
+    //da testare
+    public String Nouns(){
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public Nouns(String[] words){
+        random = new Random();
+    }
+    
+    public String[] getNouns(int count){
+        String[] pickedWords = new String[count];
+        List<String> copyList = new ArrayList<String>(words);
+        for(int i  = 0; i < count ; i++){
+            int pos = random.nextInt(copyList.size());
+            pickedWords[i] = copyList.get(pos);
+            copyList.remove(pos);
+        } 
+        return pickedWords;
+    }
 }
