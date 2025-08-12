909c97e84a2413cb3e37f7802c01c8d55c8d0d3c
alessiomodonesi <alessiomodo2004@gmail.com>
2025-08-12 18:37:02 +0200

    finite chiamate apicaller, abbozzati analyzer e validator, tutto funzionante

--- src/main/java/com/gmms/Validator.java
+++ src/main/java/com/gmms/Validator.java
@@ -1,5 +1,42 @@
 package com.gmms;
 // Alessio Modonesi
-public class Validator {
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Validator {
+    public static void verifyToxicity(String sentenceDesc) throws Exception {
+        // Invocazione della funzione getToxicityAnalysis dalla classe ApiCaller
+        String toxicityAnalysis = ApiCaller.getToxicityAnalysis(sentenceDesc);
+        double toxicityLevel = 0.0;
+        // System.out.println(toxicityAnalysis);
+
+        // Analizza la stringa JSON per ottenere l'oggetto principale
+        JsonObject rootObject = JsonParser.parseString(toxicityAnalysis).getAsJsonObject();
+
+        // Estrai l'array "moderationCategories"
+        JsonArray moderationCategories = rootObject.getAsJsonArray("moderationCategories");
+
+        // Itera sull'array per trovare l'oggetto con il nome "Toxic"
+        if (moderationCategories != null) {
+            for (int i = 0; i < moderationCategories.size(); i++) {
+                JsonObject category = moderationCategories.get(i).getAsJsonObject();
+
+                // Controlla se il nome è "Toxic"
+                if ("Toxic".equals(category.get("name").getAsString())) {
+                    toxicityLevel = category.get("confidence").getAsDouble();
+                    break;
+                }
+            }
+        }
+
+        if (toxicityLevel >= 0.50) {
+            System.out.println("error: toxicity level is too high");
+            // IOController.showToxicityError();
+        } else {
+            System.out.println("toxicity level is: " + toxicityLevel);
+            // IOController.showToxicityResults(toxicityLevel);
+        }
+    }
 }
