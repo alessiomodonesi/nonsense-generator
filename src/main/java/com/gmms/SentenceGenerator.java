d40c8b11b60ff6e0d0dfb95697ed701fa85a0301
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2025-08-13 13:45:18 +0200

    costruito lo scheletro di SentenceGenerator

--- src/main/java/com/gmms/SentenceGenerator.java
+++ src/main/java/com/gmms/SentenceGenerator.java
@@ -1,5 +1,64 @@
 package com.gmms;
+
+import java.util.Map;
+import java.util.List;
+import java.util.Random;
+
 // Diego Marchini
 public class SentenceGenerator {
+
+    String sentenceDesc;
+    Map<String, List<String>> fillingWords;
+
+    public SentenceGenerator() {
+    }
+
+    // metodi di supporto non presenti nel design class model (metodi di
+    // SentenceGenerator)
+    public void generateSentenceDesc() {
+        this.getTemplateDesc();
+        this.getWords();
+        Random r = new Random();
+        String[] wordsCategories = (String[]) (fillingWords.keySet()).toArray();// array contenente le categorie di
+                                                                                // parole
+                                                                                // ([noun],[verb],[adjective],...)
+        // interi usati per scorrere lungo l'array sopra
+        int categoryIndex = 0;
+        int categoryTotalNumber = wordsCategories.length;
+        // while annidato -> per ogni categoria di parola (cambiata ad ogni iterazione
+        // del ciclo esterno) viene ricercata la parola indicante la
+        // categoria(es."[noun]") nella descriprion e sostituita con un termine di
+        // quella categoria scelto randomicamente dal dizionario(Map)
+        while (categoryIndex < categoryTotalNumber) {
+            String wordToBeReplaced = wordsCategories[categoryIndex];
+            List<String> sameTypeWords = fillingWords.get(wordToBeReplaced);
+            int dimension = sameTypeWords.size();
+            while (dimension > 0) {// basandosi sul fatto che il WordPicker ha scelto il corretto numero di
+                                   // nomi/parole
+                // (gli # mancanti nel template)
+                int index = r.nextInt(dimension);
+                String wordToBeInserted = sameTypeWords.get(index);
+                sentenceDesc.replaceFirst(wordToBeReplaced, wordToBeInserted);
+                sameTypeWords.remove(index);
+                dimension--;
+            }
+            categoryIndex++;
+        }
+    }
+
+    // metodi di supporto non presenti nel design class model (chiamate ad altri
+    // sottosistemi)
+
+    private void getTemplateDesc() {
+        sentenceDesc = TemplateController.getTemplateDesc();
+    }
+
+    private void getWords() {
+        fillingWords = WordPicker.getWords();
+    }
+
+    public void createSentence() {
+        SentenceProcessor.createSentence(sentenceDesc);
+    }
 
 }
