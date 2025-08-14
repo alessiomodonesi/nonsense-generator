63b810eda5837207b9939431c96792c5e6bc103d
Tommaso Silvestrin <tomsilvestrin11@gmail.com>
2025-08-14 21:25:08 +0200

    Update Template.java

--- src/main/java/com/gmms/Template.java
+++ src/main/java/com/gmms/Template.java
@@ -12,22 +12,22 @@ public class Template {
     public static final int VERB_INDEX = 1;
     public static final int ADJECTIVE_INDEX = 2;
 
-    private static final Map<Integer, List<int[]>> combinations = Map.of(
+    private static final Map<Integer, List<int[]>> alternatives = Map.of(
         1, List.of (
-            new int []{NOUN_INDEX},
-            new int []{VERB_INDEX},
-            new int []{ADJECTIVE_INDEX}
+            new int [] {NOUN_INDEX},
+            new int [] {VERB_INDEX},
+            new int [] {ADJECTIVE_INDEX}
         ),
         2, List.of (
-            new int []{NOUN_INDEX, VERB_INDEX},
-            new int []{NOUN_INDEX, ADJECTIVE_INDEX},
-            new int []{ADJECTIVE_INDEX, NOUN_INDEX}
+            new int [] {NOUN_INDEX, VERB_INDEX},
+            new int [] {NOUN_INDEX, ADJECTIVE_INDEX},
+            new int [] {ADJECTIVE_INDEX, NOUN_INDEX}
         ),
         3, List.of (
-            new int []{NOUN_INDEX, VERB_INDEX, ADJECTIVE_INDEX},
-            new int []{NOUN_INDEX, ADJECTIVE_INDEX, VERB_INDEX},
-            new int []{ADJECTIVE_INDEX, NOUN_INDEX, VERB_INDEX},
-            new int []{ADJECTIVE_INDEX, NOUN_INDEX, ADJECTIVE_INDEX}
+            new int [] {NOUN_INDEX, VERB_INDEX, ADJECTIVE_INDEX},
+            new int [] {NOUN_INDEX, ADJECTIVE_INDEX, VERB_INDEX},
+            new int [] {ADJECTIVE_INDEX, NOUN_INDEX, VERB_INDEX},
+            new int [] {ADJECTIVE_INDEX, NOUN_INDEX, ADJECTIVE_INDEX}
         )
     );
 
@@ -38,39 +38,33 @@ public class Template {
     // costruttore che riceve una struttura di frase casuale e crea un oggetto
     private Template(String randomStructure) {
 
-        this.templateWords = new int[3];
+        templateWords = new int[3];
         StringBuilder temporaryTemplate = new StringBuilder(); // template in formazione
-        Random randomGenerator = new Random();
+        Random rand = new Random();
 
         String[] parts = randomStructure.split("%s", -1);
 
         for (int i = 0; i < parts.length - 1; i++) {
+            
             temporaryTemplate.append(parts[i]);
+            int dim = rand.nextInt(3) + 1; // genera un numero casuale tra 1 e 3 per determinare la dimensione della combinazione
+            List<int[]> combinations = alternatives.get(dim); // ottiene le combinazioni possibili per la dimensione generata
+            int [] alternativesChoice = combinations.get(rand.nextInt(combinations.size())); // seleziona una combinazione casuale
 
-            int size = randomGenerator.nextInt(3) + 1; // genera un numero casuale tra 1 e 3 per determinare la dimensione della combinazione
-
-            List<int[]> possibleCombinations = combinations.get(size); // ottiene le combinazioni possibili per la dimensione generata
-            int [] randomCombination = possibleCombinations.get(randomGenerator.nextInt(possibleCombinations.size())); // seleziona una combinazione casuale
-
-            for (int j = 0; j < randomCombination.length; j++) {
-                int wordType = randomCombination[j]; // ottiene il tipo di parola dalla combinazione casuale
-
-                this.templateWords[wordType]++; // incrementa il conteggio del tipo di parola
+            for (int j = 0; j < alternativesChoice.length; j++) {
+                int wordIndex = alternativesChoice[j]; // ottiene il tipo di parola dalla combinazione casuale
+                templateWords[wordIndex]++; // incrementa il conteggio del tipo di parola
                 
-                switch (wordType) {
+                switch (wordIndex) {
                     case NOUN_INDEX:
-                        temporaryTemplate.append("[NOUN]");
+                        temporaryTemplate.append("[NOUN] ");
                         break;
                     case VERB_INDEX:
-                        temporaryTemplate.append("[VERB]");
+                        temporaryTemplate.append("[VERB] ");
                         break;
                     case ADJECTIVE_INDEX:
-                        temporaryTemplate.append("[ADJECTIVE]");
+                        temporaryTemplate.append("[ADJECTIVE] ");
                         break;
-                }
-
-                if (j < randomCombination.length - 1) {
-                    temporaryTemplate.append(" ");
                 }
             }
         }
