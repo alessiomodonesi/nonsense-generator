cabee17a4ee0cc958b572aabf6c6de466963e93b
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2025-08-16 16:35:59 +0200

    SentenceGenertor funzionante

--- src/main/java/com/gmms/SentenceGenerator.java
+++ src/main/java/com/gmms/SentenceGenerator.java
@@ -1,7 +1,6 @@
 package com.gmms;
 
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -21,15 +20,12 @@ public class SentenceGenerator {
         getWords();
         Random r = new Random();
 
-        /*
-        // String[] wordsCategories = (String[]) (fillingWords.keySet()).toArray();// array contenente le categorie di
-                                                                                // parole
-                                                                                // ([noun],[verb],[adjective],...)
         String[] wordsCategories = fillingWords.keySet().toArray(new String[fillingWords.keySet().size()]);
 
         // interi usati per scorrere lungo l'array sopra
         int categoryIndex = 0;
         int categoryTotalNumber = wordsCategories.length;
+
         // while annidato -> per ogni categoria di parola (cambiata ad ogni iterazione
         // del ciclo esterno) viene ricercata la parola indicante la
         // categoria(es."[noun]") nella descriprion e sostituita con un termine di
@@ -38,45 +34,18 @@ public class SentenceGenerator {
             String wordToBeReplaced = wordsCategories[categoryIndex];
             List<String> sameTypeWords = fillingWords.get(wordToBeReplaced);
             int dimension = sameTypeWords.size();
+
             while (dimension > 0) {// basandosi sul fatto che il WordPicker ha scelto il corretto numero di
                                    // nomi/parole
                 // (gli # mancanti nel template)
                 int index = r.nextInt(dimension);
                 String wordToBeInserted = sameTypeWords.get(index);
-                sentenceDesc.replaceFirst(wordToBeReplaced, wordToBeInserted);
+                sentenceDesc = sentenceDesc.replaceFirst(wordToBeReplaced, wordToBeInserted);
                 sameTypeWords.remove(index);
                 dimension--;
             }
             categoryIndex++;
         }
-        */
-
-        // Itera direttamente su ogni coppia "categoria -> lista di parole" nella mappa.
-        for (Map.Entry<String, List<String>> entry : fillingWords.entrySet()) {
-        
-            String wordToBeReplaced = entry.getKey(); // La categoria, es: "[[noun]]"
-            
-            // IMPORTANTE: Creo una copia della lista per poter rimuovere le parole usate
-            // senza modificare la mappa originale "fillingWords".
-            List<String> sameTypeWords = new ArrayList<>(entry.getValue());
-            
-            int dimension = sameTypeWords.size();
-
-            while (dimension > 0) { // Basandosi sul fatto che il WordPicker ha scelto il corretto numero di nomi/parole
-                
-                // Scegli una parola casuale dalla lista della categoria corrente
-                int index = r.nextInt(dimension);
-                String wordToBeInserted = sameTypeWords.get(index);
-                
-                // Sostituisci la prima occorrenza della categoria con la parola scelta
-                sentenceDesc.replaceFirst(wordToBeReplaced, wordToBeInserted);
-                
-                // Rimuovi la parola usata dalla lista temporanea per non sceglierla di nuovo
-                sameTypeWords.remove(index);
-                dimension--;
-            }
-        }
-
         createSentence();
     }
 
